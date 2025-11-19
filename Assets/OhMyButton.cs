4c5002a22e065a3d4ec267eca73559a6acb214e5
hirossan4049 <haruto405329@gmail.com>
2025-11-19 14:17:22 +0900

    add btn

---
+++ Assets/OhMyButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class OhMyButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Oh my button was clicked!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
